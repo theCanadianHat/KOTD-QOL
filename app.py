af1674c60f2935361f7bfddc2bebc44e5a5d031b
Noah Herron <jayssbronze@gmail.com>
2025-11-18 01:08:05 -0500

    added version footer

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import subprocess
 from datetime import datetime
 
 from flask import Flask, render_template, jsonify
@@ -10,10 +11,19 @@ app = Flask(__name__)
 def datetimeformat(value):
     dt = datetime.utcfromtimestamp(value)
     return dt.strftime('%Y-%m-%d %H:%M UTC')
+
+def get_git_version():
+    try:
+        version = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+        return version.decode("utf-8").strip()
+    except:
+        return "dev"
+
 @app.route('/')
 def index():
     data = get_active_bosses()
-    return render_template('index.html',bosses=data)
+    version = get_git_version()
+    return render_template('index.html',bosses=data, version=version)
 
 @app.route('/api/bosses')
 def bosses():
