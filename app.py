6d121693bfae20a38882a4eeea936360f6b5f650
Noah Herron <jayssbronze@gmail.com>
2025-11-19 22:47:25 -0500

    remove tag from version uncomment req install command

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import subprocess
-from datetime import datetime
+from datetime import datetime, timezone
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
+
 from reddit import get_active_bosses
 
 app = Flask(__name__)
@@ -9,39 +9,26 @@ app = Flask(__name__)
 
 @app.template_filter('datetimeformat')
 def datetimeformat(value):
-    dt = datetime.utcfromtimestamp(value)
+    dt = datetime.fromtimestamp(value, tz=timezone.utc)
     return dt.strftime('%Y-%m-%d %H:%M UTC')
 
 def get_version():
     try:
         with open("version.txt") as f:
             lines = [line.strip() for line in f if line.strip()]
-            if len(lines) >= 3:
-                tag, hash, buildTime = lines[0], lines[1], lines[2]
-                return f"{tag}.{hash[:7]}-{buildTime}"
+            if len(lines) >= 2:
+                commit_hash, build_time = lines[0], lines[1]
+                return f"{commit_hash[:7]}-{build_time}"
             else:
                 raise RuntimeError('Version not found')
     except:
         return "dev"
-
-    with open("version.txt") as f:
-        lines = [line.strip() for line in f if line.strip()]
-    if len(lines) >= 2:
-        tag, commit = lines[0], lines[1]
-        return f"{tag} ({commit[:7]})"
-    return "unknown"
-
 
 @app.route('/')
 def index():
     data = get_active_bosses()
     version = get_version()
     return render_template('index.html',bosses=data, version=version)
-
-# @app.route('/api/bosses')
-# def bosses():
-#     data = get_active_bosses()
-#     return render_template('index.html',bosses=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-git fetch --unshallow --tags || git fetch --tags
-tag=$(git describe --tag --abbrev=0)
-echo "${tag}" > version.txt
 hash=$(git rev-parse HEAD)
-echo "${hash}" >> version.txt
+echo "${hash}" > version.txt
 buildTime=$(date +"%Y-%m-%d_%H:%M:%S") >> version.txt
 echo "${buildTime}" >> version.txt
 echo "Build Info:"
-echo -e "\tTag: ${tag}"
 echo -e "\tCommit Hash: ${hash}"
 echo -e "\tBuild Time: ${buildTime}"
-#./venv/Scripts/pip.exe install -r requirements.txt
+./venv/Scripts/pip.exe install -r requirements.txt
