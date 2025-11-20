fe8347910eb5d40505e4c38e1967b7439141e74a
Noah Herron <jayssbronze@gmail.com>
2025-11-19 22:11:02 -0500

    fix version capture for deployed app

--- app.py
+++ app.py
@@ -12,25 +12,30 @@ def datetimeformat(value):
     dt = datetime.utcfromtimestamp(value)
     return dt.strftime('%Y-%m-%d %H:%M UTC')
 
-def get_git_version():
+def get_version():
     try:
-        # Get latest tag
-        tag = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
-        tag = tag.decode("utf-8").strip()
-
-        # Get short commit hash
-        commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
-        commit = commit.decode("utf-8").strip()
-
-        return f"{tag}.{commit}"
+        with open("version.txt") as f:
+            lines = [line.strip() for line in f if line.strip()]
+            if len(lines) >= 3:
+                tag, hash, buildTime = lines[0], lines[1], lines[2]
+                return f"{tag}.{hash[:7]}-{buildTime}"
+            else:
+                raise RuntimeError('Version not found')
     except:
         return "dev"
+
+    with open("version.txt") as f:
+        lines = [line.strip() for line in f if line.strip()]
+    if len(lines) >= 2:
+        tag, commit = lines[0], lines[1]
+        return f"{tag} ({commit[:7]})"
+    return "unknown"
 
 
 @app.route('/')
 def index():
     data = get_active_bosses()
-    version = get_git_version()
+    version = get_version()
     return render_template('index.html',bosses=data, version=version)
 
 # @app.route('/api/bosses')
---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+tag=$(git describe --tag --abbrev=0)
+echo "${tag}" > version.txt
+hash=$(git rev-parse HEAD)
+echo "${hash}" >> version.txt
+buildTime=$(date +"%Y-%m-%d_%H:%M:%S") >> version.txt
+echo "${buildTime}" >> version.txt
+echo "Build Info:"
+echo -e "\tTag: ${tag}"
+echo -e "\tCommit Hash: ${hash}"
+echo -e "\tBuild Time: ${buildTime}"
+#./venv/Scripts/pip.exe install -r requirements.txt
